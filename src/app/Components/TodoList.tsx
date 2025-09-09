571d384d574f6d728c2bf1ea184111b8cbd20045
Grey-B <gabriellemener@gmail.com>
2025-09-09 10:33:46 -0300

    Criado useState em TodoList.tsx para lidar com a lista de afazeres

--- src/app/Components/TodoList.tsx
+++ src/app/Components/TodoList.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+
 const TodoList = () => {
+   const [todos, setTodos] = React.useState<string[]>(["afazer 1", "afazer 2"]);
+
    return (
       <>
          <h2 className="text-xl">Afazeres</h2>
+
+         <ul>
+            {todos.map((t, index) => (
+               <li key={index}>{t}</li>
+            ))}
+         </ul>
       </>
    );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import TodoList from "./Components/TodoList";
 
 const Home = () => {
