471467eb076bfdc2661b271490754190e3099730
Grey-B <gabriellemener@gmail.com>
2025-09-09 10:44:37 -0300

    Adicionado input e botão para adicionar afazer em TodoList.tsx

--- src/app/Components/TodoList.tsx
+++ src/app/Components/TodoList.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa";
 
 const TodoList = () => {
-   const [todos, setTodos] = React.useState<string[]>(["afazer 1", "afazer 2"]);
+   const [todos, setTodos] = React.useState<string[]>([]);
+   const [todoName, setTodoName] = React.useState<string>("");
+
+   const handleAdd = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      const newTodos: string[] = [...todos, todoName];
+      setTodos(newTodos);
+      setTodoName("");
+   };
 
    return (
       <>
          <h2 className="text-xl">Afazeres</h2>
+
+         <form className="flex items-center gap-2">
+            <input
+               onChange={(e) => setTodoName(e.target.value)}
+               value={todoName}
+               type="text"
+               placeholder="Afazer"
+               className="border rounded-md p-1"
+            />
+
+            <button
+               onClick={handleAdd}
+               aria-label="Adicionar afazer"
+               className="cursor-pointer bg-cyan-300 rounded-md shadow-md p-2"
+            >
+               <FaPlus />
+            </button>
+         </form>
 
          <ul>
             {todos.map((t, index) => (
