555fb969b587cdce50196324c4758c198236a78c
Grey-B <gabriellemener@gmail.com>
2025-09-11 09:41:17 -0300

    Criado componente UserStats.tsx, com parágrafos para nível e experiência

---
+++ src/app/Components/UserStats.tsx
@@ -0,0 +1,10 @@
+const UserStats = () => {
+   return (
+      <>
+         <p>Nível: 1</p>
+         <p>Experiência:</p>
+      </>
+   );
+};
+
+export default UserStats;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 import HabitList from "./Components/HabitList";
 import RemoveAllItems from "./Components/RemoveAllItems";
 import TodoList from "./Components/TodoList";
+import UserStats from "./Components/UserStats";
 
 const Home = () => {
    return (
       <>
          <h1 className="text-3xl font-bold">Quest Tracker</h1>
          <RemoveAllItems />
+         <br />
+         <UserStats />
          <br />
          <HabitList />
          <TodoList />
