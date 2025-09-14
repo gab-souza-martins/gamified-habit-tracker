c31a757be5810aa72b1f7b2198e119440c1e9c32
Grey-B <gabriellemener@gmail.com>
2025-09-14 10:38:03 -0300

    Criado useState em Shop.tsx para a lista de itens, que agora é usado para mostrar os itens. Também foi criado botão de comprar, ainda sem funcionalidade

--- src/app/Components/Shop.tsx
+++ src/app/Components/Shop.tsx
@@ -1,14 +1,35 @@
+import React from "react";
+import { FaCoins } from "react-icons/fa6";
+import ShopItem from "../Types/ShopItem";
+
 const Shop = () => {
+   const [itemList, setItemList] = React.useState<ShopItem[]>([
+      { id: crypto.randomUUID(), name: "Guloseima", cost: 5 },
+      { id: crypto.randomUUID(), name: "Videogame (30 min)", cost: 10 },
+   ]);
+
    return (
       <>
          <h2 className="text-xl">Loja</h2>
 
-         <div className="flex items-center gap-5">
-            <span>Guloseima</span>
-            <span>
-               <strong>Custo</strong>: 5
-            </span>
-         </div>
+         {itemList.map((i) => (
+            <div key={i.id} className="flex items-center gap-3">
+               <span>{i.name}</span>
+               <span>
+                  <strong>Custo:</strong> {i.cost}
+               </span>
+               <button
+                  //    onClick={() => setIsAddFormOpen(true)}
+                  aria-label="Comprar item"
+                  className="cursor-pointer rounded-md flex items-center gap-2 py-1 px-2
+                         bg-amber-300 shadow-sm hover:bg-amber-400 hover:shadow-xl transition duration-75 ease-in-out
+                         active:bg-amber-500 active:shadow-md focus:outline-2 focus:outline-amber-300 focus:outline-offset-2"
+               >
+                  <FaCoins />
+                  <span>Comprar</span>
+               </button>
+            </div>
+         ))}
       </>
    );
 };
---
+++ src/app/Types/ShopItem.tsx
@@ -0,0 +1,7 @@
+interface ShopItem {
+   id: string;
+   name: string;
+   cost: number;
+}
+
+export default ShopItem;
