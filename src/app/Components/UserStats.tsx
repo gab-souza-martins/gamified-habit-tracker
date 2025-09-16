4e95673ca6756caf36d08b89b050567a101912cf
Grey-B <gabriellemener@gmail.com>
2025-09-16 10:51:05 -0300

    Barras de exp agora funcionam conforme o esperado. Também percebi que estava commitando direto no main :P

--- src/app/Components/UserStats.tsx
+++ src/app/Components/UserStats.tsx
@@ -13,20 +13,44 @@ const UserStats: React.FC<UserStatsProps> = ({ stats }) => {
             Corpo {stats.body.level}:
             <div className="border rounded-md w-40">
                <div
-                  className={`rounded-md py-1 px-2 bg-yellow-300 w-${stats.body.exp}/${stats.body.expToNextLevel}`}
+                  className="rounded-md py-1 px-2 bg-yellow-300"
+                  style={{
+                     width: `calc(${stats.body.exp} / ${stats.body.expToNextLevel} * 100%)`,
+                  }}
                >
                   {stats.body.exp}/{stats.body.expToNextLevel}
                </div>
             </div>
          </div>
-         <p>
-            Mente {stats.mind.level}: {stats.mind.exp}/
-            {stats.mind.expToNextLevel}
-         </p>
-         <p>
-            Espírito {stats.spirit.level}: {stats.spirit.exp}/
-            {stats.spirit.expToNextLevel}
-         </p>
+
+         <div className="flex items-center gap-2">
+            Mente {stats.mind.level}:
+            <div className="border rounded-md w-40">
+               <div
+                  className="rounded-md py-1 px-2 bg-yellow-300"
+                  style={{
+                     width: `calc(${stats.mind.exp} / ${stats.mind.expToNextLevel} * 100%)`,
+                  }}
+               >
+                  {stats.mind.exp}/{stats.mind.expToNextLevel}
+               </div>
+            </div>
+         </div>
+
+         <div className="flex items-center gap-2">
+            Espírito {stats.spirit.level}:
+            <div className="border rounded-md w-40">
+               <div
+                  className="rounded-md py-1 px-2 bg-yellow-300"
+                  style={{
+                     width: `calc(${stats.spirit.exp} / ${stats.spirit.expToNextLevel} * 100%)`,
+                  }}
+               >
+                  {stats.spirit.exp}/{stats.spirit.expToNextLevel}
+               </div>
+            </div>
+         </div>
+
          <p className="flex items-center gap-2">
             <FaCoins /> {stats.coins}
          </p>
