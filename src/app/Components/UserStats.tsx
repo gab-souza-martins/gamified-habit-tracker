ee0deefc69043ffd136ca85f48c1f3b1e50407ec
Grey-B <gabriellemener@gmail.com>
2025-09-19 11:06:08 -0300

    UserStats agora é organizado em um flex, com divisões entre cada item

--- src/app/Components/UserStats.tsx
+++ src/app/Components/UserStats.tsx
@@ -8,53 +8,55 @@ interface UserStatsProps {
 
 const UserStats: React.FC<UserStatsProps> = ({ stats }) => {
    return (
-      <>
-         <div className="flex items-center gap-2">
-            <FaHandFist /> Corpo {stats.body.level}
-            <div className="border rounded-md w-40">
-               <div
-                  className="rounded-md p-1 bg-yellow-300"
-                  style={{
-                     width: `calc(${stats.body.exp} / ${stats.body.expToNextLevel} * 100%)`,
-                  }}
-               >
-                  {stats.body.exp}/{stats.body.expToNextLevel}
+      <div className="flex items-center gap-2 divide-x-2 divide-solid divide-neutral-400">
+         <div className="flex items-center gap-3 pr-2 divide-x-2 divide-solid divide-neutral-400">
+            <div className="flex items-center gap-2 pr-3">
+               <FaHandFist /> Corpo {stats.body.level}
+               <div className="border rounded-md w-40">
+                  <div
+                     className="rounded-md p-1 bg-yellow-300"
+                     style={{
+                        width: `calc(${stats.body.exp} / ${stats.body.expToNextLevel} * 100%)`,
+                     }}
+                  >
+                     {stats.body.exp}/{stats.body.expToNextLevel}
+                  </div>
+               </div>
+            </div>
+
+            <div className="flex items-center gap-2 pr-3">
+               <FaBrain /> Mente {stats.mind.level}
+               <div className="border rounded-md w-40">
+                  <div
+                     className="rounded-md p-1 bg-yellow-300"
+                     style={{
+                        width: `calc(${stats.mind.exp} / ${stats.mind.expToNextLevel} * 100%)`,
+                     }}
+                  >
+                     {stats.mind.exp}/{stats.mind.expToNextLevel}
+                  </div>
+               </div>
+            </div>
+
+            <div className="flex items-center gap-2 pr-3">
+               <FaHeart /> Espírito {stats.spirit.level}
+               <div className="border rounded-md w-40">
+                  <div
+                     className="rounded-md p-1 bg-yellow-300"
+                     style={{
+                        width: `calc(${stats.spirit.exp} / ${stats.spirit.expToNextLevel} * 100%)`,
+                     }}
+                  >
+                     {stats.spirit.exp}/{stats.spirit.expToNextLevel}
+                  </div>
                </div>
             </div>
          </div>
 
-         <div className="flex items-center gap-2">
-            <FaBrain /> Mente {stats.mind.level}
-            <div className="border rounded-md w-40">
-               <div
-                  className="rounded-md p-1 bg-yellow-300"
-                  style={{
-                     width: `calc(${stats.mind.exp} / ${stats.mind.expToNextLevel} * 100%)`,
-                  }}
-               >
-                  {stats.mind.exp}/{stats.mind.expToNextLevel}
-               </div>
-            </div>
-         </div>
-
-         <div className="flex items-center gap-2">
-            <FaHeart /> Espírito {stats.spirit.level}
-            <div className="border rounded-md w-40">
-               <div
-                  className="rounded-md p-1 bg-yellow-300"
-                  style={{
-                     width: `calc(${stats.spirit.exp} / ${stats.spirit.expToNextLevel} * 100%)`,
-                  }}
-               >
-                  {stats.spirit.exp}/{stats.spirit.expToNextLevel}
-               </div>
-            </div>
-         </div>
-
-         <p className="flex items-center gap-2">
+         <p className="flex items-center gap-2 pl-2">
             <FaCoins /> {stats.coins}
          </p>
-      </>
+      </div>
    );
 };
 
